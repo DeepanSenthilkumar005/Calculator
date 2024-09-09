5097879bf57315a88fd41223665e6fcf59215e64
DeepanSenthilkumar005 <deepancse45@gmail.com>
2024-09-09 15:37:02 +0530

    feat:added resul.jsx

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import result from './components/result.jsx'
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>hello world</h2>
+      {/* <button value='' onClick={<result.jsx/>}>hifkgjgc</button> */}
     </div>
   );
 }
---
+++ react-js/src/components/result.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function result (){
+    <h1>hai</h1>
+}
+export default result;
