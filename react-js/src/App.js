ee1dc41fe976e1e80f0812bb51f0a3261fa564df
DeepanSenthilkumar005 <deepancse45@gmail.com>
2024-09-12 14:58:54 +0530

    Calculator

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-// import result from './components/result.jsx'
+import Result from './components/result.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h2>hello world</h2>
-      {/* <button value='' onClick={<result.jsx/>}>hifkgjgc</button> */}
+      <Result />
     </div>
   );
 }
--- react-js/src/components/result.jsx
+++ react-js/src/components/result.jsx
@@ -1,5 +1,58 @@
-import React from "react";
-function result (){
-    <h1>hai</h1>
-}
-export default result;
+import React, { useState } from 'react';
+
+function Result() {
+  const [output, setOutput] = useState("");
+  function display(num){
+    setOutput(...num.value)
+  }
+  function clear(){
+    setOutput=null;
+  }
+  // function equal_to(){
+  //   try {
+      
+  //   } catch (error) {
+      
+  //   }
+  }
+  
+
+  return (
+    <>
+      
+      <div className='container'>
+          <div className='display'>
+            {output}
+          </div>
+          <div className='buttons'>
+              <button onClick={display('1')}>1</button>
+              <button onClick={display('2')}>2</button>
+              <button onClick={display('3')}>3</button>
+              <button onClick={display('+')}>+</button>
+              
+              <br></br>
+              <button onClick={display('4')}>4</button>
+              <button onClick={display('5')}>5</button>
+              <button onClick={display('6')}>6</button>   
+              <button onClick={display('-')}>-</button>   
+              <br></br>
+              <button onClick={display('7')}>7</button>
+              <button onClick={display('8')}>8</button>
+              <button onClick={display('9')}>9</button>   
+              <button onClick={display('*')}>*</button>           
+              <br></br>
+              <button onClick={display('.')}>.</button>
+              <button onClick={display('0')}>0</button>
+              <button onClick={clear()}>CLR</button>   
+              <button onClick={display('/')}>/</button>  
+              <br></br>                       
+              <button onClick={equal_to()}>=</button>
+          </div>
+      </div>
+    
+    
+    </>
+  );
+
+
+export default Result;
