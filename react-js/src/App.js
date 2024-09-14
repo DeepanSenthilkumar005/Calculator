7f4f49ec25d0bd788d8b90853e6d3408b33e2b75
DeepanSenthilkumar005 <deepancse45@gmail.com>
2024-09-14 21:46:26 +0530

    index in front

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Result from './components/result.jsx';
-import NotFound from './components/NotFound';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import NotFound from './components/NotFound';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App m-auto">
