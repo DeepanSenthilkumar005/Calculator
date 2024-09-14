db3c73c9b3600d4992bba0c6db03eb0e9df8ab67
DeepanSenthilkumar005 <deepancse45@gmail.com>
2024-09-14 21:28:52 +0530

    feat:Notfound page added

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import Result from './components/result.jsx';
-
+import NotFound from './components/NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
+    <div className="App m-auto">
       <Result />
     </div>
   );
---
+++ react-js/src/components/NotFound.js
@@ -0,0 +1,18 @@
+// src/components/NotFound.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the Calculator page you're looking for has an erro Please try again later</p>
+      <Link to="/">Go Back to Home</Link>
+      <a href='https://github.com/DeepanSenthilkumar005'>GitHub</a>
+
+    </div>
+  );
+};
+
+export default NotFound;
